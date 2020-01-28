190a3dd7a601504c11ccdc02e661ce2179fd6b3a
demisquare <demigliomaria@libero.it>
2020-01-28 14:45:55 +0100

    DBManager

--- src/main/java/ingsw/bullet/server/persistence/Main.java
+++ src/main/java/ingsw/bullet/server/persistence/Main.java
@@ -1,8 +1,9 @@
 package ingsw.bullet.server.persistence;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("no");
+        DBManager.getInstance();
     }
 }
