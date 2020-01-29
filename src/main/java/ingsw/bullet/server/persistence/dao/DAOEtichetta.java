ede98ca12b1ca9208258c3c95f2dfa710e0d48fe
demisquare <demigliomaria@libero.it>
2020-01-29 23:04:38 +0100

    DAO calendario ed etichetta

--- src/main/java/ingsw/bullet/server/persistence/dao/DAOEtichetta.java
+++ src/main/java/ingsw/bullet/server/persistence/dao/DAOEtichetta.java
@@ -1,31 +1,160 @@
 package ingsw.bullet.server.persistence.dao;
 
+import ingsw.bullet.server.model.Calendario;
+import ingsw.bullet.server.model.Etichetta;
+import ingsw.bullet.server.persistence.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class DAOEtichetta implements DAOInterface<DAOEtichetta> {
+public class DAOEtichetta implements DAOInterface<Etichetta> {
 
-	public void save(DAOEtichetta t) {
-		// TODO Auto-generated method stub
+	private DataSource dataSource;
+
+	public DAOEtichetta(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public void save(Etichetta t) {
+		Connection connection = null;
+
+		try {
+			connection = this.dataSource.getConnection();
+			PreparedStatement statement;
+			String query = "INSERT INTO etichetta (colore, nome) VALUES (?,?);";
+			statement = connection.prepareStatement(query);
+
+			statement.setInt(1, t.getColore());
+			statement.setString(2, t.getNome());
+
+			statement.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
+	}
+
+	public Etichetta findByPrimaryKey(Object... keys) {
+		Connection connection = null;
+		Etichetta etichetta = null;
+
+		// parser chiavi primarie...
+		String id_etichetta = (String)keys[0];
+
+		try {
+			connection = this.dataSource.getConnection();
+			PreparedStatement statement;
+			String query = "SELECT * FROM etichetta WHERE etichetta.id_etichetta = ?";
+			statement = connection.prepareStatement(query);
+			statement.setString(1, id_etichetta);
+			ResultSet result = statement.executeQuery();
+
+			if (result.next()) {
+				etichetta = new Etichetta();
+				etichetta.setId_etichetta(result.getInt("id_etichetta"));
+				etichetta.setColore(result.getInt("colore"));
+				etichetta.setNome(result.getString("nome"));
+
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
+		return etichetta;
+	}
+
+	public List<Etichetta> findAll() {
+		Connection connection = null;
+		List<Etichetta> etichette = new ArrayList<Etichetta>();
+
+		try {
+			connection = this.dataSource.getConnection();
+			Etichetta etichetta;
+			PreparedStatement statement;
+			String query = "SELECT * FROM etichetta";
+			statement = connection.prepareStatement(query);
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				etichetta = new Etichetta();
+				etichetta.setId_etichetta(result.getInt("id_etichetta"));
+				etichetta.setColore(result.getInt("colore"));
+				etichetta.setNome(result.getString("nome"));
+				etichette.add(etichetta);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
+		return etichette;
+	}
+
+	public void update(Etichetta t) {
+		Connection connection = null;
+
+		try {
+			connection = this.dataSource.getConnection();
+			PreparedStatement statement;
+			String query = "UPDATE etichetta SET" +
+					"etichetta.etichetta = ?, " +
+					"etichetta.colore = ?, " +
+					"etichetta.nome = ?";
+			statement = connection.prepareStatement(query);
+
+			statement.setInt(1, t.getId_etichetta());
+			statement.setInt(2, t.getColore());
+			statement.setString(3, t.getNome());
+
+			statement.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
 		
 	}
 
-	public DAOEtichetta findByPrimaryKey(Object... keys) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<DAOEtichetta> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void update(DAOEtichetta t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void delete(DAOEtichetta t) {
-		// TODO Auto-generated method stub
+	public void delete(Etichetta t) {
+		Connection connection = null;
+		try {
+			connection = this.dataSource.getConnection();
+			String delete = "DELETE FROM etichetta WHERE etichetta.id_etichetta = ?";
+			PreparedStatement statement = connection.prepareStatement(delete);
+			statement.setInt(1, t.getId_etichetta());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
 		
 	}
     
