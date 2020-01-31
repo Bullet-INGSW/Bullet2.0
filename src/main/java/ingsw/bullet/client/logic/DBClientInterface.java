e22a23a4d8913993e4cde29f099838857f1f39a1
MartinaCanonPh <marti2f.mc@gmail.com>
2020-01-31 15:38:16 +0100

    added remove() methods in DBInterface

--- src/main/java/ingsw/bullet/client/logic/DBClientInterface.java
+++ src/main/java/ingsw/bullet/client/logic/DBClientInterface.java
@@ -11,22 +11,27 @@ public interface DBClientInterface {
     Calendario findCalendarioById(int id);
     Calendario insertCalendario(Calendario c);
     Calendario updateCalendario(Calendario c);
+    boolean removeCalendario(int id);
 
     Etichetta findEtichettaById(int id);
     Etichetta insertEtichetta(Etichetta c);
     Etichetta updateEtichetta(Etichetta c);
+    boolean removeEtichetta(int id);
 
     Evento findEventoById(int id);
     Evento insertEvento(Etichetta c);
     Evento updateEvento(Etichetta c);
+    boolean removeEvento(int id);
 
     Gruppo findGruppoById(int id);
     Gruppo insertGruppo(Gruppo c);
     Gruppo updateGruppo(Gruppo c);
+    boolean removeGruppo(int id);
 
     Membro findMembroById(String email);
     Membro insertMembro(Membro c);
     Membro updateMembro(Membro c);
+    boolean removeMembro(int id);
 
     Notifica findNotificaById(int id);
     Notifica insertNotifica(Notifica c);
@@ -35,16 +40,20 @@ public interface DBClientInterface {
     Promemoria findPromemoriaById(int id);
     Promemoria insertPromemoria(Promemoria c);
     Promemoria updatePromemoria(Promemoria c);
+    boolean removePromemoria(int id);
 
     TDL findTDLById(int id);
     TDL insertTDL(TDL c);
     TDL updateTDL(TDL c);
+    boolean removeTDL(int id);
 
-    Utente findUtenteById(String email);
+    Utente findUtenteByEmail(String email);
     Utente insertUtente(Utente c);
     Utente updateUtente(Utente c);
+    boolean removeUtente(String email);
 
-    Partecipante findPromemoriaById(String email);
-    Partecipante insertPromemoria(Partecipante c);
-    Partecipante updatePromemoria(Partecipante c);
+    Partecipante findPartecipanteByEmail(String email);
+    Partecipante insertPartecipante(Partecipante c);
+    Partecipante updatePartecipante(Partecipante c);
+    boolean removePartecipante(String email);
 }
