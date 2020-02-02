313365e195b393ad6d23f9038d4f018b8cd91bde
MartinaCanonPh <marti2f.mc@gmail.com>
2020-02-02 16:09:17 +0100

    added creaTDL fxml e controller

---
+++ src/main/java/ingsw/bullet/client/logic/controllerFXML/CreaTDL.java
@@ -0,0 +1,38 @@
+package ingsw.bullet.client.logic.controllerFXML;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreaTDL {
+
+    @FXML
+    private Button indietro;
+
+    @FXML
+    private Button conferma;
+
+    @FXML
+    private Button nuovoPromemoria;
+
+    @FXML
+    private TextField nomeTDL;
+
+    @FXML
+    void creaNuovoPromemoria(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvaTDL(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tornaIndietro(ActionEvent event) {
+
+    }
+
+}
+
