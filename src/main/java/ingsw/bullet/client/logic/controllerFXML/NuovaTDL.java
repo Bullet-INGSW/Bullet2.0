5fa46898a0e3f3e3fb76863ae6193746d4dc11d7
MartinaCanonPh <marti2f.mc@gmail.com>
2020-02-02 16:39:25 +0100

    imported model.TDL

--- src/main/java/ingsw/bullet/client/logic/controllerFXML/NuovaTDL.java
+++ src/main/java/ingsw/bullet/client/logic/controllerFXML/NuovaTDL.java
@@ -1,14 +1,16 @@
 package ingsw.bullet.client.logic.controllerFXML;
 
+import ingsw.bullet.model.TDL;
+
 public class NuovaTDL extends NuovoGestoreAttivita {
 
     @Override
     public void aggiungiGestoreAttivita() {
-//        TDL c = new TDL();
-//        c.setNome(nomeGestoreAttivita.getText());
-//        c.setIdGruppo(gruppoId);
-//        DBClient.getIstance().insertTDL(c);
-//
-//        Main.getInstance().replaceSceneContent("sceltaTDLCondivise", Main.getInstance().stage, 800, 600);
+        TDL c = new TDL();
+        c.setNome(nomeGestoreAttivita.getText());
+        c.setIdGruppo(gruppoId);
+       // DBClient.getIstance().insertTDL(c);
+
+      // Main.getInstance().replaceSceneContent("sceltaTDLCondivise", Main.getInstance().stage, 800, 600);
     }
 }
